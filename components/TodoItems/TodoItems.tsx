09bfccc134db3ebeda77a221b8b1296b3207a5d1
developer <ch9716@naver.com>
2024-05-23 16:25:11 +0900

    초기 파일 추가 및 스타일 세팅.

---
+++ components/TodoItems/TodoItems.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const TodoItems = () => {
+  return (
+    <View>
+      <Text>TodoItems</Text>
+    </View>
+  );
+};
+
+export default TodoItems;
---
+++ components/TodoMain/TodoMain.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const TodoMain = () => {
+  return (
+    <View>
+      <Text>TodoMain</Text>
+    </View>
+  );
+};
+
+export default TodoMain;
