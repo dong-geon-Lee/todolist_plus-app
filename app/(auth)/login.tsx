f7c9f05bb1b7f79f346302e61118ed57438a0f45
developer <ch9716@naver.com>
2024-05-24 15:24:11 +0900

    로그인 로직 적용 및 메인페이지로 이동처리.

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,20 +1,68 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import React from "react";
-import { Link } from "expo-router";
+import React, { useRef, useState } from "react";
+import { router } from "expo-router";
 
+import assets from "@/assets";
 import * as S from "./styles";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const passwordInputRef = useRef<any>(null);
+
+  const onSumbit = () => {
+    console.log("제출");
+    console.log(email, password);
+    router.push("/home");
+  };
+
   return (
-    <SafeAreaView>
-      <S.Container>
-        <View>
-          <Text>Login</Text>
-          <Link href="/home">로그인</Link>
-        </View>
-      </S.Container>
-    </SafeAreaView>
+    <S.Container>
+      <S.Main>
+        <S.HeaderView>
+          <S.ImageMain source={assets.heros} />
+        </S.HeaderView>
+
+        <S.Section>
+          <S.Box>
+            <S.LabelEmail>이메일</S.LabelEmail>
+            <S.Div>
+              <S.Img source={assets.mail} alt="mail" />
+              <S.Input
+                autoCapitalize="none"
+                keyboardType="email-address"
+                returnKeyType="next"
+                value={email}
+                onChangeText={setEmail}
+                placeholder="yourLife@gmail.com"
+                placeholderTextColor="#dbdbdb"
+                onSubmitEditing={() => passwordInputRef.current.focus()}
+                blurOnSubmit={false}
+              />
+            </S.Div>
+          </S.Box>
+          <S.Box>
+            <S.LabelEmail>비밀번호</S.LabelEmail>
+            <S.Div>
+              <S.Img source={assets.lock} alt="lock" />
+              <S.Input
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+                placeholder="password"
+                placeholderTextColor="#dbdbdb"
+                returnKeyType="done"
+                ref={passwordInputRef}
+                onSubmitEditing={onSumbit}
+              />
+            </S.Div>
+          </S.Box>
+        </S.Section>
+
+        <S.Button onPress={onSumbit}>
+          <S.Title>로그인</S.Title>
+        </S.Button>
+      </S.Main>
+    </S.Container>
   );
 };
 
--- app/(auth)/styles.ts
+++ app/(auth)/styles.ts
@@ -1,3 +1,118 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Platform, ScrollView } from "react-native";
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled(SafeAreaView)`
+  padding-left: 24px;
+`;
+
+export const Main = styled(ScrollView)`
+  padding-right: 18px;
+`;
+
+export const HeaderView = styled.View`
+  width: 343px;
+  height: 211px;
+  margin-top: 97px;
+`;
+
+export const Section = styled.View``;
+
+export const Box = styled.View``;
+
+export const Img = styled.Image`
+  width: 20px;
+  height: 20px;
+  flex-shrink: 0;
+  margin-left: 10px;
+  margin-right: 4px;
+`;
+
+export const LogoImg = styled.Image``;
+
+export const Div = styled.View`
+  width: 343px;
+  height: 42px;
+  flex-shrink: 0;
+  border-radius: 4px;
+  border: 2px solid #dbdbdb;
+  margin-bottom: 26px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const BoxEmail = styled.View``;
+
+export const EmailText = styled.Text``;
+
+export const PwdText = styled.Text``;
+
+export const BoxPwd = styled.View``;
+
+export const LabelEmail = styled.Text`
+  color: #999;
+  text-align: left;
+  font-family: "Inter-Medium";
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 20px; /* 142.857% */
+  letter-spacing: -0.5px;
+  margin-bottom: 4px;
+`;
+
+export const LabelPwd = styled.Text``;
+
+export const Input = styled.TextInput<any>`
+  color: #111;
+  text-align: left;
+  font-family: "Inter-Medium";
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 20px; /* 142.857% */
+  letter-spacing: -0.5px;
+  width: 100%; /* 전체 너비 사용 */
+  height: 100%; /* 전체 높이 사용 */
+  padding: 0 4px; /* 패딩 추가 */
+`;
+
+export const ImageMain = styled.Image`
+  width: 100%;
+  height: 100%;
+  flex-shrink: 0;
+  object-fit: cover;
+  display: block;
+`;
+
+export const Button = styled.Pressable`
+  width: 343px;
+  height: 63px;
+  flex-shrink: 0;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  background: #016bdf;
+  margin-top: 77px;
+  ${Platform.OS === "ios"
+    ? `
+    shadow-color: rgba(1, 107, 223, 0.2);
+    shadow-offset: 0px 4px;
+    shadow-opacity: 1;
+    shadow-radius: 20px;
+  `
+    : `
+    elevation: 4;
+  `}
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  text-align: center;
+  font-family: "Inter-SemiBold";
+  font-size: 14px;
+  font-style: normal;
+  line-height: 20px; /* 142.857% */
+  letter-spacing: -0.5px;
+`;
